78c9035db5c128381a4b005b47000c354ae48648
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2019-04-08 00:28:44 +0200

    04-json

---
+++ workshop/04-json/src/main/java/com/infoshareacademy/App.java
@@ -0,0 +1,42 @@
+package com.infoshareacademy;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * 04-json
+ */
+public class App {
+    private static final String FILE_NAME = "users.json";
+
+    public static void main(String[] args) {
+
+        JSONParser parser = new JSONParser();
+
+        try {
+            // <editor-fold desc="Read user's emails">
+            ClassLoader classLoader = App.class.getClassLoader();
+            File file = new File(Objects.requireNonNull(classLoader.getResource(FILE_NAME)).getFile());
+            Object obj = parser.parse(new FileReader(file.getAbsolutePath()));
+            JSONArray users = (JSONArray) obj;
+            List<String> emails = (List<String>) users.stream().map(u -> ((JSONObject) u).get("email").toString()).collect(Collectors.toList());
+            // </editor-fold>
+
+            // Find users having e-mails in domain .pl
+            emails.forEach(mail -> {
+                if (mail.contains("pl")) {
+                    System.out.println(mail);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
