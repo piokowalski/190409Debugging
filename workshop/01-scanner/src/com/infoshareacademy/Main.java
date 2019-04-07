eac1090faee978ac6eeca951fff348587b1857f0
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2019-04-08 00:06:41 +0200

    01-scanner

---
+++ workshop/01-scanner/src/com/infoshareacademy/Main.java
@@ -0,0 +1,25 @@
+package com.infoshareacademy;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Read number a from standard input
+        System.out.print("a = ");
+        int a = scanner.nextInt();
+
+        // Read number b from standard input
+        System.out.print("b = ");
+        int b = scanner.nextInt();
+
+        // Output a + b
+        int c = a + b;
+        System.out.println("a + b = " + c);
+
+        scanner.close();
+    }
+}
