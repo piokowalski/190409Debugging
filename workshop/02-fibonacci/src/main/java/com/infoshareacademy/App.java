d721af68fba49b5d102afd14b3e3892be4cd174f
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2019-04-08 00:12:22 +0200

    02-fibonacci

---
+++ workshop/02-fibonacci/src/main/java/com/infoshareacademy/App.java
@@ -0,0 +1,30 @@
+package com.infoshareacademy;
+
+import java.util.Scanner;
+
+/**
+ * 02-fibonacci.
+ * This application prints out the first n Fibonacci numbers.
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n = ");
+        int n = scanner.nextInt();
+
+        int a = 0;
+        int b = 1;
+        int c;
+
+        for (int i = 1; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+            System.out.println(String.format("Fib%d = %d", i, c));
+        }
+
+        scanner.close();
+    }
+}
